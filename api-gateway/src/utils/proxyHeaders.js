6615252fc69505ac621bd6fd152d08630614ee91
Chamuditha Subharath <subharathc@gmail.com>
2026-03-11 00:00:17 +0530

    feat(gateway): add reusable proxy header builder for downstream services

---
+++ api-gateway/src/utils/proxyHeaders.js
@@ -0,0 +1,32 @@
+const getProxyHeaders = (req, extra = {}) => {
+  const headers = {
+    'Content-Type': 'application/json'
+  };
+
+  if (req.headers.authorization) {
+    headers.Authorization = req.headers.authorization;
+  }
+
+  if (req.headers['x-user-id']) {
+    headers['X-User-ID'] = req.headers['x-user-id'];
+  }
+
+  if (req.headers['x-user-email']) {
+    headers['X-User-Email'] = req.headers['x-user-email'];
+  }
+
+  if (req.headers['x-user-role']) {
+    headers['X-User-Role'] = req.headers['x-user-role'];
+  }
+
+  if (req.headers['x-request-id']) {
+    headers['X-Request-ID'] = req.headers['x-request-id'];
+  }
+
+  return {
+    ...headers,
+    ...extra
+  };
+};
+
+module.exports = getProxyHeaders;
