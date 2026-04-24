1c1960e81bcfaf4aebab20a6e96dc7f64de11763
Chamuditha Subharath <subharathc@gmail.com>
2026-04-24 08:42:44 +0530

    refactor(frontend): remove wrongly placed files

--- final/src/api/users.js
+++
@@ -1,7 +0,0 @@
-import client from './client'
-
-export const getUser = (userId) =>
-  client.get(`/users/${userId}`)
-
-export const updateUser = (userId, data) =>
-  client.put(`/users/${userId}`, data)
